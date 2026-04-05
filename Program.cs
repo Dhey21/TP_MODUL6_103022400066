f470f53db90326cd029ef548c267837c7f603d93
Dhey <dddzul779@gmail.com>
2026-04-05 20:19:11 +0700

    menambahkan implementasi design by contract

--- Program.cs
+++ Program.cs
@@ -6,16 +6,25 @@
 
     public SayaMusicTrack(string title)
     {
+        if (title == null)
+            throw new ArgumentException("Judul tidak boleh null");
+        if (title.Length > 100)
+            throw new ArgumentException("Judul maksimal 100 karakter");
         this.title = title;
-
         Random rand = new Random();
         this.id = rand.Next(10000, 100000);
-
         this.playCount = 0;
     }
 
     public void IncreasePlayCount(int count)
     {
+        if (count < 0)
+            throw new ArgumentException("Tidak boleh negatif");
+        if (count > 10000000)
+            throw new ArgumentException("Maksimal 10 juta");
+        if ((long)playCount + count > int.MaxValue)
+            throw new OverflowException("Play count overflow");
+
         playCount += count;
     }
 
@@ -30,15 +39,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        SayaMusicTrack lagu1 = new SayaMusicTrack("Risk it all");
-        SayaMusicTrack lagu2 = new SayaMusicTrack("About you");
+        try
+        {
+            SayaMusicTrack lagu1 = new SayaMusicTrack("Risk it all");
+            SayaMusicTrack lagu2 = new SayaMusicTrack("About you");
 
-        lagu1.IncreasePlayCount(77);
-        lagu1.PrintTrackDetails();
-        lagu2.IncreasePlayCount(99);
-        lagu2.PrintTrackDetails();
+            for (int i = 0; i < 5; i++)
+            {
+                lagu1.IncreasePlayCount(10000000);
+            }
+
+            lagu1.PrintTrackDetails();
+            lagu2.PrintTrackDetails();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: " + e.Message);
+        }
     }
 }
-
-
 
--- obj/Debug/net10.0/TP_MODUL6_103022400066.AssemblyInfo.cs
+++ obj/Debug/net10.0/TP_MODUL6_103022400066.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_MODUL6_103022400066")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+179166be3357441ba9df43c31c76785901a315e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf277276a994f6dd8fc93b77e7432f48103b4010")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_MODUL6_103022400066")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_MODUL6_103022400066")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
