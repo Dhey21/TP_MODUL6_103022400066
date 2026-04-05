cf277276a994f6dd8fc93b77e7432f48103b4010
Dhey <dddzul779@gmail.com>
2026-04-05 20:13:04 +0700

    “menambahkan class SayaMusicTrack

--- Program.cs
+++ Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class SayaMusicTrack
+{
+    private int id;
+    private int playCount;
+    private string title;
+
+    public SayaMusicTrack(string title)
+    {
+        this.title = title;
+
+        Random rand = new Random();
+        this.id = rand.Next(10000, 100000);
+
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        playCount += count;
+    }
+
+    public void PrintTrackDetails()
+    {
+        Console.WriteLine("ID: " + id);
+        Console.WriteLine("Title: " + title);
+        Console.WriteLine("Play Count: " + playCount);
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        SayaMusicTrack lagu1 = new SayaMusicTrack("Risk it all");
+        SayaMusicTrack lagu2 = new SayaMusicTrack("About you");
+
+        lagu1.IncreasePlayCount(77);
+        lagu1.PrintTrackDetails();
+        lagu2.IncreasePlayCount(99);
+        lagu2.PrintTrackDetails();
+    }
+}
+
+
+
--- obj/Debug/net10.0/TP_MODUL6_103022400066.AssemblyInfo.cs
+++ obj/Debug/net10.0/TP_MODUL6_103022400066.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_MODUL6_103022400066")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+903659a35efa826c6313d66e8bbcc089b84dcea6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+179166be3357441ba9df43c31c76785901a315e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_MODUL6_103022400066")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_MODUL6_103022400066")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
